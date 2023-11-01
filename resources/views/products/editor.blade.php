144fb70c03a4051e1e098b468dfe14d3efca2e8b
caleb_mwasikira <calebmwasikira@gmail.com>
2023-11-01 16:49:51 +0300

    Working on products.show and products.editor ui

--- resources/views/products/editor.blade.php
+++ resources/views/products/editor.blade.php
@@ -101,7 +101,7 @@
 
                 <div class="mt-1">
                     <textarea id="description" name="description" rows="5"
-                        class="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 outline-none placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-600 sm:text-sm sm:leading-6">{{ old('description') ?? $product->description->body }}</textarea>
+                        class="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 outline-none placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-600 sm:text-sm sm:leading-6">{{ old('description') ?? $product->description?->body }}</textarea>
                 </div>
 
                 <p class="mt-1 text-sm leading-6 text-gray-600">
